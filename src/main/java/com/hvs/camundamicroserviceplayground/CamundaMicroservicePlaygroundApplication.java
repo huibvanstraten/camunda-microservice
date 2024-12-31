29f33df50ad44ef1396fd3e0a14eb75a691509fe
huibvanstraten <huib.van.straten@ritense.com>
2024-12-31 13:05:01 +0100

    Initial commit

---
+++ src/main/java/com/hvs/camundamicroserviceplayground/CamundaMicroservicePlaygroundApplication.java
@@ -0,0 +1,13 @@
+package com.hvs.camundamicroserviceplayground;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CamundaMicroservicePlaygroundApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CamundaMicroservicePlaygroundApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/hvs/camundamicroserviceplayground/artist/service/ArtistService.java
@@ -0,0 +1,12 @@
+package com.hvs.camundamicroserviceplayground.artist.service;
+
+import org.camunda.bpm.engine.runtime.Execution;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ArtistService {
+
+    public void getArtistsForUser(Execution execution) {
+        System.out.println("Executing getArtistsForUser");
+    }
+}
